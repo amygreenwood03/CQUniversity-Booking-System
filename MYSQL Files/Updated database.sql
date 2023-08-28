7988a5e8e2d7a9da8e7f688c01e7e1c7e1b3c79c
MrBreadd <PetreAW@outlook.com>
2023-08-28 18:15:32 +1000

    Updated database script

--- MYSQL Files/Updated database.sql
+++ MYSQL Files/Updated database.sql
@@ -4,75 +4,82 @@ USE app_dev_database;
 
 -- Create the Department table
 CREATE TABLE IF NOT EXISTS Department (
-    department_id INT AUTO_INCREMENT PRIMARY KEY,
-    department_name CHAR(50) NOT NULL
+    DEPT_ID INT AUTO_INCREMENT PRIMARY KEY,
+    DEPT_NAME CHAR(50) NOT NULL
 );
 
 -- Create the Category table
 CREATE TABLE IF NOT EXISTS Category (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name CHAR(50) NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES Department(department_id)
+    CAT_ID INT AUTO_INCREMENT PRIMARY KEY,
+    CAT_NAME CHAR(50) NOT NULL,
+    DEPT_ID INT NOT NULL,
+    FOREIGN KEY (DEPT_ID) REFERENCES Department(DEPT_ID)
 );
 
 -- Create the Services table
 CREATE TABLE IF NOT EXISTS Services (
-	service_id INT AUTO_INCREMENT PRIMARY KEY,
-    service_name CHAR(50),
-    service_description CHAR(255) NOT NULL,
-    category_id INT NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES Category(category_id)
+	SERV_ID INT AUTO_INCREMENT PRIMARY KEY,
+    SERV_NAME CHAR(50) NOT NULL,
+    SERV_DESC CHAR(255) NOT NULL,
+    SERV_PRICE INT NOT NULL,
+    CAT_ID INT NOT NULL,
+    FOREIGN KEY (CAT_ID) REFERENCES Category(CAT_ID)
+);
+
+-- Create User table
+CREATE TABLE IF NOT EXISTS User (
+	USER_ID INT AUTO_INCREMENT,
+    NAME CHAR(50),
+    PHONE INT NOT NULL,
+    EMAIL CHAR(50) NOT NULL,
+	USER_TYPE CHAR(10) NOT NULL,
+    PRIMARY KEY (USER_ID)
 );
 
 -- Create the Login table
 CREATE TABLE IF NOT EXISTS Login (
-    email CHAR(50) PRIMARY KEY,
-    password CHAR(128) NOT NULL, -- allowing for SHA512 encryption
-    user_type CHAR(10) NOT NULL
+	LOGIN_ID INT AUTO_INCREMENT,
+    EMAIL CHAR(50) NOT NULL,
+    PASSWORD CHAR(128) NOT NULL, -- allowing for SHA512 encryption
+    SALT CHAR(255),
+    PRIMARY KEY (LOGIN_ID)
 );
 
 -- Create the Staff table
 CREATE TABLE IF NOT EXISTS Staff (
-    staff_id INT AUTO_INCREMENT PRIMARY KEY,
-    staff_name CHAR(50) NOT NULL,
-    email CHAR(50) UNIQUE NOT NULL,
-    staff_phone INT NOT NULL,
-    department_id INT,
-    FOREIGN KEY (email) REFERENCES Login(email),
-    FOREIGN KEY (department_id) REFERENCES Department(department_id)
-);
-
--- Create the Location table
-CREATE TABLE IF NOT EXISTS Location (
-    location_id INT AUTO_INCREMENT PRIMARY KEY,
-    location_name CHAR(50) NOT NULL,
-    location_address CHAR(50) NOT NULL
-);
-
--- Create the Services_at_location table
-CREATE TABLE IF NOT EXISTS Services_at_location (
-	sal_id INT AUTO_INCREMENT PRIMARY KEY,
-    service_id INT NOT NULL,
-    location_id INT NOT NULL,
-    FOREIGN KEY (service_id) REFERENCES Services(service_id),
-    FOREIGN KEY (location_id) REFERENCES Location(location_id)
+    USER_ID INT PRIMARY KEY,
+    DEPT_ID INT NOT NULL,
+    FOREIGN KEY (USER_ID) REFERENCES User(USER_ID),
+    FOREIGN KEY (DEPT_ID) REFERENCES Department(DEPT_ID)
 );
 
 -- Create the Volunteer table
 CREATE TABLE IF NOT EXISTS Volunteer (
-    volunteer_id INT AUTO_INCREMENT PRIMARY KEY,
-    volunteer_name CHAR(50) NOT NULL,
-    email CHAR(50) UNIQUE NOT NULL,
-    volunteer_phone INT NOT NULL,
-    FOREIGN KEY (email) REFERENCES Login(email)
+    USER_ID INT PRIMARY KEY,
+    FOREIGN KEY (USER_ID) REFERENCES User(USER_ID)
+);
+
+-- Create the Location table
+CREATE TABLE IF NOT EXISTS Location (
+    LOC_ID INT AUTO_INCREMENT PRIMARY KEY,
+    lOC_NAME CHAR(50) NOT NULL,
+    LOC_ADDR CHAR(50) NOT NULL
+);
+
+-- Create the Services_at_location table
+CREATE TABLE IF NOT EXISTS Services_at_location (
+	SAL_ID INT AUTO_INCREMENT PRIMARY KEY,
+    SERV_ID INT NOT NULL,
+    LOC_ID INT NOT NULL,
+    FOREIGN KEY (SERV_ID) REFERENCES Services(SERV_ID),
+    FOREIGN KEY (LOC_ID) REFERENCES Location(LOC_ID)
 );
 
 -- Create the Registration table
 CREATE TABLE IF NOT EXISTS Registration (
-	registration_id INT AUTO_INCREMENT PRIMARY KEY,
-    sal_id INT NOT NULL,
-    volunteer_id INT NOT NULL,
-    FOREIGN KEY (sal_id) REFERENCES Services_at_location(sal_id),
-    FOREIGN KEY (volunteer_id) REFERENCES Volunteer(volunteer_id)
+	REG_ID INT AUTO_INCREMENT PRIMARY KEY,
+    SAL_ID INT NOT NULL,
+    USER_ID INT NOT NULL,
+    FOREIGN KEY (SAL_ID) REFERENCES Services_at_location(SAL_ID),
+    FOREIGN KEY (USER_ID) REFERENCES Volunteer(USER_ID)
 );
