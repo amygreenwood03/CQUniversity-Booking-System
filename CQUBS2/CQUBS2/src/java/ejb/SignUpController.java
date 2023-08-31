5b568c4bb3239f509d6221a0fa923abff2d5e3f1
HeimannK <kahlia.heimann@cqumail.com>
2023-08-31 18:19:20 +1000

    Modified Sign Up controller and XHTML so page is visible

--- CQUBS2/CQUBS2/src/java/ejb/SignUpController.java
+++ CQUBS2/CQUBS2/src/java/ejb/SignUpController.java
@@ -14,10 +14,55 @@ import java.security.MessageDigest;
 public class SignUpController implements Serializable {
     @EJB
     private UsersEJB usersEJB;
+    private Volunteer user;
     private final String PAGE_NAME = "Volunteer Signup";
-    private String firstName, lastName, phone, email, password = ""; 
+    private String firstName, lastName, phoneNumber, emailAddress, password = ""; 
     
     public SignUpController() {
         
+    }
+    
+    public String getPAGE_NAME() {
+        return PAGE_NAME;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+    
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
